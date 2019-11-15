8769c964ff101646b4c77c9abbb76c780c3db656
t-kehiguchi <ec2-user@ip-172-31-32-142.ap-northeast-1.compute.internal>
2019-11-15 09:30:07 +0000

    追加機能No.6～No.8(モデルのバリデーション対応)完了

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,3 +1,23 @@
 class Attendance < ApplicationRecord
   belongs_to :user
+  validates :worked_on,  presence: true
+  validate :started_saved_when_finished_save
+  validate :started_earlier_than_finished
+
+  def started_saved_when_finished_save
+    if finished_at.present?
+      if !started_at.present?
+        errors.add(:started_at, "は退社時間を保存する時に存在しないといけません。")
+      end
+    end
+  end
+
+  def started_earlier_than_finished
+    if started_at.present? && finished_at.present?
+      if started_at > finished_at
+        errors.add(:started_at, "よりも遅い時間にしないといけません。")
+      end
+    end
+  end
+
 end
